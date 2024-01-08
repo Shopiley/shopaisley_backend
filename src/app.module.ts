71dbcf3bf64041e79e0c47a18e8beffb27fcad85
Fadekemi Adebayo <82163647+Shopiley@users.noreply.github.com>
2024-01-08 17:44:16 +0100

    set up db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ProductModule } from './product/product.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
+import { User } from './user/entities/user.entity';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
@@ -16,12 +17,16 @@ require('dotenv').config();
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
+      url: process.env.DB_URL,
       port: 5432,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [],
+      entities: [User],
       synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
     UserModule,
     ProductModule,
