949de364c7c7e8b9a592b2366d074a303fd1c6bd
kaobimdi iwelumo <kaobimdi.iwelumo@pau.edu.ng>
2024-01-10 17:24:53 +0100

    my code

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,26 @@
-export class User {}
+import { Auth } from 'src/auth/entities/auth.entity';
+import { Entity, Column, PrimaryGeneratedColumn, OneToOne } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  phoneNum: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @Column()
+  email: string;
+
+  @OneToOne(() => Auth, { cascade: true })
+  auth: Auth;
+}
