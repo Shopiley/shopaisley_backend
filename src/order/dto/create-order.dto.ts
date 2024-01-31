73fe84ebde8343b8caf8449280341a1b9f3ee154
Keisha2810 <keishamartins13@gmail.com>
2024-01-31 16:55:18 +0100

    Updating with dev

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,1 +1,47 @@
-export class CreateOrderDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsString,
+  IsNumber,
+} from 'class-validator';
+
+export class CreateOrderDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  unitPrice: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  SKU: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  ImageURL: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  discountId: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  categoryId: number;
+}
+
+export class CreateOrderDtoResponse extends CreateOrderDto {
+  readonly id: string;
+}
--- src/order/entities/order.entity.ts
+++ src/order/entities/order.entity.ts
@@ -1,1 +1,40 @@
-export class Order {}
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    ManyToOne,
+    JoinColumn,
+  } from 'typeorm';
+  
+  import { User } from 'src/user/entities/user.entity'; // Import User entity if it exists
+  import { Product } from 'src/product/entities/product.entity'; // Import Product entity if it exists
+  
+  @Entity()
+  export class Order {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+  
+    @Column({
+        type: 'enum',
+        enum: ['Pending', 'Confirmed', 'Out for Delivery', 'Delivered'],
+        default: 'Pending',
+    })
+    status: string;
+  
+    @Column({ type: 'timestamp'})
+    order_date: Date;
+
+    @Column({ type: 'decimal', precision: 10, scale: 2 })
+    total_amount: number;
+  
+    // Relationships
+    @ManyToOne(() => User, { eager: true, onDelete: 'CASCADE'}) 
+    @JoinColumn({ name: 'user_id' })
+    user: User;
+  
+    @ManyToOne(() => Product, { eager: true })
+    @JoinColumn({ name: 'product_id' })
+    product: Product;
+
+}
+  
